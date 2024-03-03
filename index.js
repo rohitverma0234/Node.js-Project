4c01305c1357d7f41cd5971b04a9539ceb51a3c3
rohitverma0234 <rohitverma0150@gmail.com>
2024-03-03 12:35:21 -0500

    Display Index.html on Server

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path')
 
 const Hostname = "localhost";
 const Port = 3000;
 
-const Server = http.createServer((req,res)=>{
-    console.log(req.headers)
+const Server = http.createServer((req, res) => {
+    console.log("requsest for " + req.url + " by method " + req.method)
 
-    res.statusCode=200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Server is created successfully :)</h1></body></html>')
+    if (req.method == 'GET') {
+        var fileUrl;
+        if (req.url == "/") {
+            fileUrl = "/index.html"
+        } else {
+            fileUrl = req.url
+        }
+
+        var filePath = path.resolve('./Public' + fileUrl);  // folderName and index.html
+
+        const fileExt = path.extname(filePath)
+
+        if (fileExt == ".html") {
+            fs.exists(filePath, (exists) => {     // exists = callback function
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body><h1>Error 404 ' + fileUrl + ' does not exists. </h1></body></html>')
+                }
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res)
+            })
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Error 404 ' + fileUrl + ' not a HTML file. </h1></body></html>')
+        }
+
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404 ' + fileUrl + ' not supported. </h1></body></html>')
+    }
+
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/html');
+    // res.end('<html><body><h1>Server is created successfully :)</h1></body></html>')
 })
 
-Server.listen(Port , Hostname, ()=>{          // Port should be the first argument.
+Server.listen(Port, Hostname, () => {          // Port should be the first argument.
     console.log(`server is runnung in http://${Hostname}:${Port}`)
 })
